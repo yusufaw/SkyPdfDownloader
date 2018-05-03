fa90526c7147da6b722551aa4fcd848e1b6d0777
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-05-03 22:03:25 +0700

    Add request permission write disk

--- app/src/main/java/com/skyshi/pdfdownloader/MainActivity.kt
+++ app/src/main/java/com/skyshi/pdfdownloader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.skyshi.pdfdownloader
 
+import android.Manifest
 import android.app.DownloadManager
 import android.content.Context
 import android.net.Uri
@@ -8,29 +9,32 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.EasyPermissions
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    var fileName: String? = "your-file-name.pdf"
+    private val fileName: String = "your-file-name.pdf"
+    private val stringUrl: String = "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"
     private var lastDownload = -1L
-    lateinit var dm: DownloadManager
+    private lateinit var dm: DownloadManager
+    private val requestCodeWriteDisk = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
-        button.setOnClickListener({ startDownload("https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf") })
+        button.setOnClickListener({ downloadTask() })
     }
 
-    private fun startDownload(stringUrl: String) {
+    private fun startDownload() {
         val uri = Uri.parse(stringUrl)
 
         val fi = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
                 fileName)
         if (fi.exists()) {
-//            openPdfReader()
+
         } else {
             lastDownload = dm.enqueue(DownloadManager.Request(uri)
                     .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
@@ -44,5 +48,30 @@ class MainActivity : AppCompatActivity() {
                     .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
                     .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED))
         }
+    }
+
+
+    private fun hasWriteDiskPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    private fun downloadTask() {
+        if (hasWriteDiskPermission()) {
+            startDownload()
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.rationale_disk),
+                    requestCodeWriteDisk,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+        if (hasWriteDiskPermission()) startDownload()
     }
 }
