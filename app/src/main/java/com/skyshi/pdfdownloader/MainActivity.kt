a9ebc2cf7d3e430b5294154b85c909dd09c514fa
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-06-06 09:27:33 +0700

    Moving all function download to new module

--- app/src/main/java/com/skyshi/pdfdownloader/MainActivity.kt
+++ app/src/main/java/com/skyshi/pdfdownloader/MainActivity.kt
@@ -1,59 +1,26 @@
 package com.skyshi.pdfdownloader
 
 import android.Manifest
-import android.app.DownloadManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.view.View
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import com.skyshi.skypdf.ActivityPdfRender
 import kotlinx.android.synthetic.main.activity_main.*
 import pub.devrel.easypermissions.EasyPermissions
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private val fileName: String = "your-file-name.pdf"
     private val stringUrl: String = "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"
-    //    private var lastDownload = -1L
-    private lateinit var dm: DownloadManager
     private val requestCodeWriteDisk = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        registerReceiver(onComplete,
-                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         button.setOnClickListener({ downloadTask() })
     }
 
     private fun startDownload() {
-        val uri = Uri.parse(stringUrl)
-
-        val fi = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                fileName)
-        if (fi.exists()) {
-
-        } else {
-            dm.enqueue(DownloadManager.Request(uri)
-                    .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-                    .setAllowedOverRoaming(false)
-                    .setTitle(fileName)
-                    .setVisibleInDownloadsUi(true)
-                    .setNotificationVisibility(View.VISIBLE)
-                    .setDescription("This is a description")
-                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,
-                            fileName)
-                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED))
-        }
+        ActivityPdfRender.download(this, stringUrl, fileName)
     }
 
 
@@ -79,26 +46,5 @@ class MainActivity : AppCompatActivity() {
 
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
         if (hasWriteDiskPermission()) startDownload()
-    }
-
-
-    private var onComplete: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(ctxt: Context, intent: Intent) {
-            Toast.makeText(ctxt, "Download Complete", Toast.LENGTH_LONG).show()
-            openPdfReader()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(onComplete)
-    }
-
-    private fun openPdfReader() {
-        if (fileName.isNotEmpty()) {
-            val intent = Intent(baseContext, ActivityPdfRender::class.java)
-            intent.putExtra("stringUri", fileName)
-            startActivity(intent)
-        }
     }
 }
--- skypdf/src/main/java/com/skyshi/skypdf/ActivityPdfRender.kt
+++ skypdf/src/main/java/com/skyshi/skypdf/ActivityPdfRender.kt
@@ -1,42 +1,95 @@
 package com.skyshi.skypdf
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import java.io.File
 
 const val FRAGMENT_PDF_RENDERER_BASIC = "pdf_renderer_basic"
 
 class ActivityPdfRender : AppCompatActivity() {
 
+    private lateinit var dm: DownloadManager
+    private lateinit var fileName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdf_render)
 
-//        if (savedInstanceState == null) {
+        dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        registerReceiver(onComplete,
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         val extras = intent.extras
-        val exampleString = extras.getString("stringUri")
-        supportFragmentManager.beginTransaction()
-                .add(R.id.container, PdfRendererBasicFragment.newInstance(exampleString),
-                        FRAGMENT_PDF_RENDERER_BASIC)
-                .commit()
-//        }
+
+
+        val stringUrl = extras.getString("stringUrl")
+        fileName = extras.getString("fileName")
+        startDownload(stringUrl)
     }
 
-//    fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        getMenuInflater().inflate(R.menu.main, menu)
-//        return true
-//    }
+    companion object {
+        fun download(context: Context, stringUrl: String, fileName: String) {
+            val intent = Intent(context, ActivityPdfRender::class.java)
+            intent.putExtra("stringUrl", stringUrl)
+            intent.putExtra("fileName", fileName)
+            context.startActivity(intent)
+        }
+    }
 
-//    fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.getItemId()) {
-//            R.id.action_info -> {
-//                AlertDialog.Builder(this)
-//                        .setMessage(R.string.intro_message)
-//                        .setPositiveButton(android.R.string.ok, null)
-//                        .show()
-//                return true
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
 
+    private var onComplete: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(ctxt: Context, intent: Intent) {
+            Toast.makeText(ctxt, "Download Complete", Toast.LENGTH_LONG).show()
+            openPdfReader()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(onComplete)
+    }
+
+
+    private fun openPdfReader() {
+        if (fileName.isNotEmpty()) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.addToBackStack(null)
+            transaction.replace(R.id.container, PdfRendererBasicFragment.newInstance(fileName),
+                    FRAGMENT_PDF_RENDERER_BASIC)
+            transaction.commitAllowingStateLoss()
+        }
+    }
+
+    private fun startDownload(stringUrl: String) {
+        val uri = Uri.parse(stringUrl)
+
+        val fi = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                fileName)
+        if (fi.exists()) {
+            openPdfReader()
+        } else {
+            dm.enqueue(DownloadManager.Request(uri)
+                    .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                    .setAllowedOverRoaming(false)
+                    .setTitle(fileName)
+                    .setVisibleInDownloadsUi(true)
+                    .setNotificationVisibility(View.VISIBLE)
+                    .setDescription("This is a description")
+                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,
+                            fileName)
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED))
+        }
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
 }
